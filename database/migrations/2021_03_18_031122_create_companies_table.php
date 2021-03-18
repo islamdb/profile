edd9cf71ca7d5f3380578fac3f176325d94d110b
Rafy Aulia Akbar <rafyakbar@smadia.id>
2021-03-18 10:14:23 +0700

    add migrations

---
+++ database/migrations/2021_03_18_031122_create_companies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Support\SEO;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')
+                ->unique();
+            $table->text('address');
+            $table->string('lat');
+            $table->string('lon');
+            $table->longText('body')
+                ->nullable();
+            SEO::metaColumns($table);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
