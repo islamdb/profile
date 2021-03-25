b8d4496b264554ca19c304b5eb4d29d729d1e3b0
rafy <rafy683@gmail.com>
2021-03-25 09:32:45 +0700

    update

--- app/Providers/PermissionProvider.php
+++ app/Providers/PermissionProvider.php
@@ -4,9 +4,12 @@ namespace App\Providers;
 
 use App\Models\Permission;
 use App\Support\MyField;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use Orchid\Platform\Dashboard;
 use Orchid\Platform\ItemPermission;
 
@@ -25,23 +28,36 @@ class PermissionProvider extends ServiceProvider
     /**
      * Bootstrap services.
      *
+     * @param Dashboard $dashboard
      * @return void
      */
     public function boot(Dashboard $dashboard)
     {
-        if (Schema::hasTable('permissions')) {
-            $permissionGroups = Permission::query()
-                ->select(['group', 'slug', 'name'])
-                ->get()
-                ->groupBy('group');
+        // Resource permissions
+        collect(File::allFiles(base_path('app/Orchid/Resources')))
+            ->map(function (\SplFileInfo $file) {
+                $name = str_replace('Resource.php', '', $file->getFilename());
+                $name = Str::snake($name, ' ');
 
-            foreach ($permissionGroups as $group => $permissionGroup) {
-                $itemPermission = ItemPermission::group($group);
-                foreach ($permissionGroup as $permission) {
-                    $itemPermission->addPermission($permission->slug, $permission->name);
+                $permissions = [];
+                foreach (['menu', 'browse', 'read', 'edit', 'add', 'delete'] as $item) {
+                    $permissions[] = (object)[
+                        'group' => Str::title($name),
+                        'slug' => str_replace(' ', '-', $name.'.'.$item),
+                        'name' => ucfirst($item).' '.$name,
+                    ];
                 }
+
+                return $permissions;
+            })
+            ->flatten(1)
+            ->groupBy('group')
+            ->each(function (Collection $permissions, $group) use ( &$dashboard) {
+                $itemPermission = ItemPermission::group($group);
+                $permissions->each(function ($permission) use (&$itemPermission) {
+                    $itemPermission->addPermission($permission->slug, $permission->name);
+                });
                 $dashboard->registerPermissions($itemPermission);
-            }
-        }
+            });
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(PermissionSeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(PostSeeder::class);
         $this->call(PageSeeder::class);
